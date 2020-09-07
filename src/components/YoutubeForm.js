7cac09e15f904ad6c5b16c9903bdcb68df236555
Charles Ivia <iviacharles@gmail.com>
2020-09-07 16:11:44 +0300

    Form validation

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -23,6 +23,12 @@ const YoutubeForm = () => {
 
       if (!values.email) {
         errors.email = "Required";
+      } else if (
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+          values.email
+        )
+      ) {
+        errors.email = "Invalid email address";
       }
       if (!values.channel) {
         errors.channel = "Required";
